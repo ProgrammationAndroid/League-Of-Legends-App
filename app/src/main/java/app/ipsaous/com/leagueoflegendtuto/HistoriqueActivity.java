a7b6a1fe2434286794a7b18b1b44832daff6556f
ipsaous <ipsaous@hotmail.com>
2015-08-29 13:54:18 +0200

    NavigationView + Toolbar pour HistoriqueActivity

--- app/src/main/java/app/ipsaous/com/leagueoflegendtuto/HistoriqueActivity.java
+++ app/src/main/java/app/ipsaous/com/leagueoflegendtuto/HistoriqueActivity.java
@@ -2,7 +2,10 @@ package app.ipsaous.com.leagueoflegendtuto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,7 +14,9 @@ public class HistoriqueActivity extends AppCompatActivity {
 
     private String playerName;
     private Long playerId;
-    private TextView test;
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +26,18 @@ public class HistoriqueActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
-        test = (TextView) findViewById(R.id.test);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
 
         if(extras.getString("NAME") != null && extras.getLong("ID") > 0){
-
             playerName = extras.getString("NAME");
             playerId = extras.getLong("ID");
-
-            test.setText(playerName + "/" + String.valueOf(playerId));
-
+            toolbar.setTitle(playerName);
         }
+
+        setSupportActionBar(toolbar);
+
 
     }
 
